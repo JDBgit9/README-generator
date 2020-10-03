d740b8996f83f214d10146ef781a08b34785bb47
JDBgit9 <info@jamesdbeeks.com>
2020-10-03 11:12:56 -0400

    finished the questions and got everythig working

--- index.js
+++ index.js
@@ -1,81 +1,68 @@
 // require all node packages
-var inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMArkdown = require("./utils/generateMarkdown");
+
+
 
 // set up inqurirer with all necessary questions
-inquirer
-    .prompt([
-        { 
-            message: "What is your Github username?",
-            name: "github",
-            type: "input" 
-        },
-        {
-            message: "What is your email address?",
-            name: "email",
-            type: "input"
-        },
-        {
-            message: "What is your project name?",
-            name: "title",
-            type: "input"
-        },
-        {
-            message: "How would you describe your project?",
-            name: "Description",
-            type: "input"
-        },
-        {
-            message: "What kind of license should your project have??",
-            name: "License",
-            type: "input"
-        },
-        {
-            message: "What command should be run to install the dependencies?",
-            name: "Install",
-            type: "input"
-        },
-        {
-            // name of person using, licence (MIT Apache GNy ISC) year  is for and what the use of this program 
-            message: "What is the full name of the person this license is for?",
-            name: "fullname",
-            type: "input"
-        },
-         {
-            message: "What license will the user be using?",
-            choice: "MIT", "Apache". "GNU", "ISC",
-            type: "list"
-        },
-        {
-            message: "What is the year for this license?",
-            name: "year",
-            type: "input"
-        },
-        {
-            message: "What is the purpose of this license use?",
-            name: "p",
-            type: "input"
-        },
-        
+inquirer.prompt([
+    {
+        message: "What is your project name?",
+        name: "title",
+        type: "input"
+    },
+    {
+        message: "How would you describe your project?",
+        name: "description",
+        type: "input"
+    },
+    {
+        message: "What is your Github username?",
+        name: "github",
+        type: "input"
+    },
+    {
+        message: "What year was this project created?",
+        name: "yearcreated",
+        type: "input"
+    },
+    {
+        message: "What is the full name of the user attaining the license",
+        name: "fullname",
+        type: "input"
+    },
+    {
+        message: "What command should be run to install dependencies",
+        name: "install",
+        type: "input"
+    },
+
+    {
+        message: "What is the purpose of this project?",
+        name: "usage",
+        type: "input"
+    },
+    {
+        message: "Is there anyone else to credit for this project?",
+        name: "credits",
+        type: "input"
+    },
+    {
+        message: "What is your email address?",
+        name: "email",
+        type: "input"
+    },
+    {
+        message: "What kind of license should your project have?",
+        name: "license",
+        type: "input"
+    },
+])
+    .then(function (answers) {
+        console.log(answers);
     
-             
-    ])
-    .then(answers => {
-        // Use user feedback for... whatever!!
-        console.log(answers.email)
-        console.log(answers.projectname)
-        console.log(answers.github)
-       // set up functinality to write the markdown file
-       //    make a general markdown template
-    //    Pass the answers in  as temperate literals
-
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else when wrong
-        }
-    });
-
-
-
+        const generatedFile = generateMArkdown(answers)
+    
+        console.log(generatedFile);
+    } )
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,56 @@
+function generateMArkdown(answers) {
+    return `
+# ${answers.title}
+    
+
+## Description
+${answers.description}
+
+## Github
+# ${answers.github}
+
+## Year Created
+# ${answers.yearcreated}
+
+# Full Name
+${answers.fullname}
+
+
+## Installation
+# ${answers.install}
+
+## Usage
+# ${answers.usage}
+
+
+## Sources/links
+# Provide instructions and examples for use. Include screenshots as needed. 
+
+
+## Credits
+# ${answers.credits}
+
+
+## Email
+# ${answers.email}
+
+## License 
+# ${answers.license}
+
+
+## Badges
+
+![Badmath](img.shields.io/github/license/JDBgit9/README.md)
+
+
+---
+© 2020 James D Beeks developer readme, a Delisco, Inc. brand. All Rights Reserved.
+
+
+
+
+
+    `;
+}
+
+module.exports = generateMArkdown;
